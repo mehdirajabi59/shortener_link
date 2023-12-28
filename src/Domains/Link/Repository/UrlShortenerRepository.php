5e00f7dc1421a62dc6c1b91dbfdd6c8ff6373773
Mehdi Rajabi <mehdints@gmail.com>
2023-12-28 17:06:42 +0330

    feat: use db transaction

--- src/Domains/Link/Repository/UrlShortenerRepository.php
+++ src/Domains/Link/Repository/UrlShortenerRepository.php
@@ -12,17 +12,34 @@ class UrlShortenerRepository implements IUrlShortenerRepository
     public function show(string $shortCode): UrlShortener
     {
         $tableName = static::TABLE_NAME;
-        $sth =DB::getConn()->prepare("SELECT * FROM `{$tableName}` WHERE `short_code` = ?");
-        $sth->execute([$shortCode]);
+
+        $conn = DB::getConn();
+
+        $conn->beginTransaction();
+
+        try {
+            $sth = $conn->prepare("SELECT * FROM `{$tableName}` WHERE `short_code` = ?");
+            $sth->execute([$shortCode]);
+
+            $this->increaseUsage($shortCode);
+
+        }catch (\Exception){
+            $conn->rollBack();
+        }
+
 
         $link = $sth->fetch(PDO::FETCH_ASSOC);
 
         $entity = new UrlShortener();
-
-        if ($link) {
-            $entity->setAttributes($link);
-        }
+        $entity->setAttributes($link);
 
         return $entity;
+    }
+
+    public function increaseUsage(string $shortCode)
+    {
+        $tableName = static::TABLE_NAME;
+        $sth = DB::getConn()->prepare("UPDATE {$tableName} SET `usage` = `usage` + 1 WHERE `short_code` = ?");
+        $sth->execute([$shortCode]);
     }
 }
