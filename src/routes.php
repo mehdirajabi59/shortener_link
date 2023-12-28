f615a28d6fa4721e24ea7f1b4d58ef2dd85e1d0a
Mehdi Rajabi <mehdints@gmail.com>
2023-12-28 17:10:03 +0330

    refactor: move routes to separate file

---
+++ src/routes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Mehdi\Core\Router;
+
+return [
+    Router::init(
+        'GET',
+        '/{link}',
+        [\Mehdi\ShortenerLink\Domains\Link\Controller\Go2UrlController::class, 'go']
+    ),
+    Router::init(
+        'POST',
+        '/user/sign-in',
+        [\Mehdi\ShortenerLink\Domains\Authentication\Controller\LoginController::class, '__invoke']
+    ),
+    // Access below route with token
+    Router::init(
+        'POST',
+        '/user/urls',
+        [\Mehdi\ShortenerLink\Domains\Link\Controller\UrlController::class, 'create'],
+        [\Mehdi\ShortenerLink\Domains\Shared\Middleware\AuthenticateMiddleware::class]
+    ),
+    Router::init(
+        'DELETE',
+        '/user/urls/{shortCode}',
+        [\Mehdi\ShortenerLink\Domains\Link\Controller\UrlController::class, 'delete'],
+        [\Mehdi\ShortenerLink\Domains\Shared\Middleware\AuthenticateMiddleware::class]
+    ),
+    Router::init(
+        'GET',
+        '/user/urls/all',
+        [\Mehdi\ShortenerLink\Domains\Link\Controller\UrlController::class, 'index'],
+        [\Mehdi\ShortenerLink\Domains\Shared\Middleware\AuthenticateMiddleware::class]
+    )
+];
